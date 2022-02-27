9b8a75467650f1e8ac3500ae9dbe5ed02811cbb2
Joshua Munsey <bambit@tfwno.gf>
2022-02-27 00:10:20 -0800

    Add files via upload

---
+++ portfolio/Untitled/story.js
@@ -0,0 +1,41 @@
+// A glorified JSON file.
+
+const storyData = {
+        "?": {
+            // NOTE(josh): Multi-line string does not work in actual JSON.
+            "content": [
+                `This is a non-linear story in the style of a Jupyter
+                Notebook. As such, per every paragraph of story provided, you will
+                be provided with an input box to control which direction you'd
+                like to pursue within the story. The accepted inputs will be shown in `,
+                `red`,
+                ` for each individual paragraph, as so. After entering your
+                desired input, press shift + enter to submit your decision. Try
+                it now to begin!`
+            ]
+        },
+
+        "0": {
+            "content": [ `Foo!` ],
+            "acceptedInput": {
+                "foo": "1", 
+                "bar": "1", 
+            }
+        },
+        
+        "1": {
+            "content": [ `Bar!` ],
+            "acceptedInput": {
+                "bar": "2",
+                "foo": "2",
+            }
+        },
+
+        "2": {
+            "content": [ `Baz!` ],
+            "acceptedInput": {
+                "baz": "0",
+                "bar": "1",
+            }
+        }
+};
